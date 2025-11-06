fb79489e6fe0150191cf1183c13432e5afaffb54
Lucas Bonvin <lucasbonvin@hotmail.com>
2025-11-06 11:38:26 +0100

    feature: clean code, add license, add dependencies

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # midi-usb-console
+
+## Requirements
+
+### Arduino Libraries
+
+#### USB MIDI
+
+- MIDIUSB v1.0.5 By Gary Grewal, Arduino
+
+#### Bluetooth MIDI
+
+- ArduinoBLE v1.4.1 By Arduino
+- BLE-MIDI v2.2 By lathoub
+- MIDI Library v5.0.2 By Francois Best, lathoub
+
+## License
+
+This project is licensed under the [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/) License. You may use, modify, and share it for non-commercial purposes.
--- midi-usb-console/midi-usb-console.ino
+++ midi-usb-console/midi-usb-console.ino
@@ -1,60 +1,88 @@
-/*
- * MIDIUSB_test.ino
- *
- * Created: 4/6/2015 10:47:08 AM
- * Author: gurbrinder grewal
- * Modified by Arduino LLC (2015)
- */
+// comment out those define if you don't want one of the midi transports
+#define BLUETOOTH_MIDI
+#define USB_MIDI
+#define TRS_MIDI
 
+#ifdef USB_MIDI
 #include "MIDIUSB.h"
+#endif
 
+#ifdef BLUETOOTH_MIDI
 #include <BLEMIDI_Transport.h>
 static const char *const SERVICE_UUID = "03b80e5a-ede8-4b33-a751-6ce34ec4c700";
 static const char *const CHARACTERISTIC_UUID = "7772e5db-3868-4112-a1a9-f2669d106bf3";
 #include <hardware/BLEMIDI_ArduinoBLE.h>
 
+BLEMIDI_CREATE_INSTANCE("ble-midi-console", MidiBluetooth)
+#endif
 
-BLEMIDI_CREATE_INSTANCE("ble-midi-console", MIDI)
-
-#define PIN_INDEX 0
-#define CC_CHANNEL_INDEX 1
-
+// number of digital and analog channels
 #define DIGITAL_CHANNELS_NUMBER 11
 #define ANALOG_CHANNELS_NUMBER 8
 
+// pin number for the midi activity led
 #define LED_MIDI_PIN 13
 
+// minimum time the led stays on after a control change
 #define MIN_LED_TIME_MS 20
 
-uint8_t analog_channels[ANALOG_CHANNELS_NUMBER][2] = { { A0, 10 }, { A1, 11 }, { A2, 12 }, { A3, 13 }, { A4, 14 }, { A5, 15 }, { A6, 16 }, { A7, 17 } };
-uint8_t digital_channels[DIGITAL_CHANNELS_NUMBER][2] = { { 9, 100 }, { 8, 101 }, { 7, 102 }, { 6, 103 }, { 5, 104 }, { 4, 105 }, { 3, 106 }, { 2, 107 }, { 12, 108 }, { 11, 109 }, { 10, 110 } };
+/**
+ * @brief Structure to map a pin to a cc number
+ */
+struct pin_cc
+{
+  uint8_t pin;
+  uint8_t cc;
+};
 
-uint8_t digital_value[DIGITAL_CHANNELS_NUMBER] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-uint8_t analog_value[ANALOG_CHANNELS_NUMBER] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+// index for pin and cc in the pin_cc structure, both analog (potentiomenters, 0..127) and digital (switches, 0..1)
+struct pin_cc analog_channels[ANALOG_CHANNELS_NUMBER] = {{A0, 10}, {A1, 11}, {A2, 12}, {A3, 13}, {A4, 14}, {A5, 15}, {A6, 16}, {A7, 17}};
+struct pin_cc digital_channels[DIGITAL_CHANNELS_NUMBER] = {{9, 100}, {8, 101}, {7, 102}, {6, 103}, {5, 104}, {4, 105}, {3, 106}, {2, 107}, {12, 108}, {11, 109}, {10, 110}};
 
+// current values of the channels
+uint8_t digital_value[DIGITAL_CHANNELS_NUMBER] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t analog_value[ANALOG_CHANNELS_NUMBER] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+// last time a control change was sent
 unsigned long lastControlChange = 0;
+// flag to indicate if the led is on because of a control change
 bool controlChangeLed = false;
 
+// smoothing factor for analog inputs
 const float SMOOTHING = 0.05;
 
+// status of bluetooth midi connection
 bool bluetoothMidiConnected = false;
 
+/**
+ * @brief Send a midi control change on all available transports
+ *
+ * @param channel midi channel (0..15)
+ * @param control control number (0..127)
+ * @param value control value (0..127)
+ */
+void controlChange(byte channel, byte control, byte value)
+{
 
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
+#ifdef USB_MIDI
+  sendControlChangeUSB(channel, control, value);
+#endif
 
-void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = { 0x0B, 0xB0 | channel, control, value };
-  MidiUSB.sendMIDI(event);
-  sendCC(channel, control, value);
+#ifdef TRS_MIDI
+  sendControlChangeTRS(channel, control, value);
+#endif
 
-  if (bluetoothMidiConnected) {
-    MIDI.sendControlChange(control, value, channel);
+  if (bluetoothMidiConnected) // when bluetooth midi is connected, activity led is inverted
+  {
+
+#ifdef BLUETOOTH_MIDI
+    sendControlChangeBluetooth(channel, control, value);
+#endif
 
     digitalWrite(LED_MIDI_PIN, 0);
-  } else {
+  }
+  else
+  {
 
     digitalWrite(LED_MIDI_PIN, 1);
   }
@@ -63,117 +91,192 @@ void controlChange(byte channel, byte control, byte value) {
   controlChangeLed = 1;
 }
 
-void sendCC(byte channel, byte control, byte value) {
+/**
+ * @brief Send a midi control change over usb midi
+ *
+ * @param channel midi channel (0..15)
+ * @param control control number (0..127)
+ * @param value control value (0..127)
+ */
+void sendControlChangeUSB(byte channel, byte control, byte value)
+{
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+/**
+ * @brief Send a midi control change over trs midi
+ *
+ * @param channel midi channel (0..15)
+ * @param control control number (0..127)
+ * @param value control value (0..127)
+ */
+void sendControlChangeTRS(byte channel, byte control, byte value)
+{
   byte status = 0xB0 | ((channel - 1) & 0x0F);
   Serial1.write(status);
   Serial1.write(control);
   Serial1.write(value);
 }
 
-void initDigitals() {
+/**
+ * @brief Send a midi control change over bluetooth midi
+ *
+ * @param channel midi channel (0..15)
+ * @param control control number (0..127)
+ * @param value control value (0..127)
+ */
+void sendControlChangeBluetooth(byte channel, byte control, byte value)
+{
+  MidiBluetooth.sendControlChange(control, value, channel);
+}
+
+/**
+ * @brief Initialize digital inputs
+ *
+ */
+void initDigitals()
+{
   uint8_t tmpValue;
-  for (int i = 0; i < DIGITAL_CHANNELS_NUMBER; i++) {
-    pinMode(digital_channels[i][PIN_INDEX], INPUT_PULLUP);
-    tmpValue = digitalRead(digital_channels[i][PIN_INDEX]);
+  for (int i = 0; i < DIGITAL_CHANNELS_NUMBER; i++)
+  {
+    pinMode(digital_channels[i].pin, INPUT_PULLUP);
+    tmpValue = digitalRead(digital_channels[i].pin);
     digital_value[i] = tmpValue;
-    controlChange(0, digital_channels[i][CC_CHANNEL_INDEX], tmpValue);
+    controlChange(0, digital_channels[i].cc, tmpValue);
   }
 
   pinMode(LED_MIDI_PIN, OUTPUT);
 }
 
-void readDigitals() {
+/**
+ * @brief Read digital inputs and send midi control changes if value changed
+ *
+ */
+void readDigitals()
+{
   uint8_t tmpValue;
-  for (int i = 0; i < DIGITAL_CHANNELS_NUMBER; i++) {
-    tmpValue = digitalRead(digital_channels[i][PIN_INDEX]);
+  for (int i = 0; i < DIGITAL_CHANNELS_NUMBER; i++)
+  {
+    tmpValue = digitalRead(digital_channels[i].pin);
 
-    if (tmpValue != digital_value[i]) {
+    if (tmpValue != digital_value[i])
+    {
       digital_value[i] = tmpValue;
-      controlChange(0, digital_channels[i][CC_CHANNEL_INDEX], tmpValue);
+      controlChange(0, digital_channels[i].cc, tmpValue);
     }
   }
 }
 
-
-void initAnalog() {
+/**
+ * @brief Initialize analog inputs
+ *
+ */
+void initAnalog()
+{
   uint16_t rawValue;
   float filtered;
 
-  for (int i = 0; i < ANALOG_CHANNELS_NUMBER; i++) {
-    // read 10-bit value, reduce to 7 bits
-    rawValue = analogRead(analog_channels[i][PIN_INDEX]) >> 3;
-
+  for (int i = 0; i < ANALOG_CHANNELS_NUMBER; i++)
+  {
+    // read 10-bit value, reduce to 7 bits (midi goes from 0 to 127 so 7 bits)
+    rawValue = analogRead(analog_channels[i].pin) >> 3;
 
     analog_value[i] = rawValue;
-    controlChange(0, analog_channels[i][CC_CHANNEL_INDEX], rawValue);
+    controlChange(0, analog_channels[i].cc, rawValue);
   }
 }
 
-void readAnalogs() {
+/**
+ * @brief Read analog inputs and send midi control changes if value changed
+ *
+ */
+void readAnalogs()
+{
   uint16_t rawValue;
+  uint16_t newValue;
+
   float filtered;
 
-  for (int i = 0; i < ANALOG_CHANNELS_NUMBER; i++) {
-    // read 10-bit value, reduce to 7 bits
-    rawValue = analogRead(analog_channels[i][PIN_INDEX]) >> 3;
-
+  for (int i = 0; i < ANALOG_CHANNELS_NUMBER; i++)
+  {
+    // read 10-bit value, reduce to 7 bits (midi goes from 0 to 127 so 7 bits)
+    rawValue = analogRead(analog_channels[i].pin) >> 3;
 
     // exponential smoothing
     filtered = (float)analog_value[i] * (1.0 - SMOOTHING) + (float)rawValue * SMOOTHING;
-    uint16_t newValue;
 
     if (filtered > 64)
-      newValue = ceil(filtered);  // to be sure to reach 127
+      newValue = ceil(filtered); // to be sure to reach 127, round to upper value
     else
-      newValue = floor(filtered);  // to be sure to reach 0
+      newValue = floor(filtered); // to be sure to reach 0, round to lower value
 
     // only send if value changed (integer step)
-    if (newValue != analog_value[i]) {
+    if (newValue != analog_value[i])
+    {
       analog_value[i] = newValue;
-      controlChange(0, analog_channels[i][CC_CHANNEL_INDEX], newValue);
+      controlChange(0, analog_channels[i].cc, newValue);
     }
   }
 }
 
-
-void setup() {
+/**
+ * @brief Arduino setup function
+ *
+ */
+void setup()
+{
   Serial.begin(115200);
 
-  BLEMIDI.setHandleConnected([]() {
+#ifdef BLUETOOTH_MIDI
+  // bluetooth callbacks for connection status
+  BLEMidiBluetooth.setHandleConnected([]()
+                                      {
     digitalWrite(LED_MIDI_PIN, 1);
-    bluetoothMidiConnected = true;
-  });
+    bluetoothMidiConnected = true; });
 
-  BLEMIDI.setHandleDisconnected([]() {
+  BLEMidiBluetooth.setHandleDisconnected([]()
+                                         {
     digitalWrite(LED_MIDI_PIN, 0);
-    bluetoothMidiConnected = false;
-  });
+    bluetoothMidiConnected = false; });
 
-  MIDI.begin();
+  MidiBluetooth.begin();
+#endif
+
+  // wait for midi connection before init --> init send current value of all controls
   delay(2000);
+
+#ifdef TRS_MIDI
   Serial1.begin(31250);
+#endif
+
   initDigitals();
   initAnalog();
 }
 
+/**
+ * @brief Arduino main loop
+ *
+ */
+void loop()
+{
 
-
-void loop() {
-
-  if (MIDI.read()) {
-    Serial.println(MIDI.getType());
-  }
+#ifdef BLUETOOTH_MIDI
+  MidiBluetooth.read();
+#endif
 
   readDigitals();
   readAnalogs();
 
-  if (millis() - lastControlChange > MIN_LED_TIME_MS && controlChangeLed == 1) {
-
-
+  // turn off midi activity led after a minimum time
+  if (millis() - lastControlChange > MIN_LED_TIME_MS && controlChangeLed == 1)
+  {
+    // when bluetooth midi is connected, activity led is inverted
     if (bluetoothMidiConnected)
       digitalWrite(LED_MIDI_PIN, 1);
     else
       digitalWrite(LED_MIDI_PIN, 0);
+
     controlChangeLed = 0;
   }
 }
